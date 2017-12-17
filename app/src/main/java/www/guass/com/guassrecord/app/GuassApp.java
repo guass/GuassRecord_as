af9d852dc8c77965002aae38def436c84e6150d1
guass <624512733@qq.com>
2017-12-17 23:14:10 +0800

    add xutils

--- app/src/main/java/www/guass/com/guassrecord/app/GuassApp.java
+++ app/src/main/java/www/guass/com/guassrecord/app/GuassApp.java
@@ -2,6 +2,8 @@ package www.guass.com.guassrecord.app;
 
 import android.app.Application;
 
+import org.xutils.x;
+
 /**
  * Created by guass on 2017/12/17.
  */
@@ -12,5 +14,11 @@ public class GuassApp extends Application {
     public void onCreate()
     {
         super.onCreate();
+        init();
+    }
+
+    private void init(){
+        x.Ext.init(this);
+        x.Ext.setDebug(false); // 是否输出debug日志, 开启debug会影响性能.
     }
 }
