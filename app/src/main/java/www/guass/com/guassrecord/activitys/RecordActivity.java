4df8293271e0ea1dc434e27b0a4245a8f99c40c2
guass <624512733@qq.com>
2018-01-03 14:17:33 +0800

    1.add item view

--- app/src/main/java/www/guass/com/guassrecord/activitys/RecordActivity.java
+++ app/src/main/java/www/guass/com/guassrecord/activitys/RecordActivity.java
@@ -1,11 +1,18 @@
 package www.guass.com.guassrecord.activitys;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import www.guass.com.guassrecord.R;
@@ -39,5 +46,35 @@ public class RecordActivity extends SwipeBackActivity {
             }
         });
 
+    }
+
+    private void  showItemDetail(){
+        View contentView = LayoutInflater.from(this).inflate(0, null);
+
+        AlertDialog dialog_pop_alert= new AlertDialog.Builder(this).create();
+        dialog_pop_alert.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
+        });
+
+        dialog_pop_alert.setCancelable(false);
+        dialog_pop_alert.setCanceledOnTouchOutside(false);
+        Window window = dialog_pop_alert.getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        dialog_pop_alert.show();
+        window.getDecorView().setPadding(0, 0, 0, 0);
+        window.setGravity(Gravity.CENTER);
+        window.setContentView(contentView);
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.width = 800;
+        lp.height = 600;
+        window.setAttributes(lp);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
